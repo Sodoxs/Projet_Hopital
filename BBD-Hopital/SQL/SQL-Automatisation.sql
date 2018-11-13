323e2f6e63d83f5baa04561696c853d579b8a0f7
Guillaume GARCIA <37486861+Sodoxs@users.noreply.github.com>
2018-11-13 16:25:35 +0100

    Update SQL-Automatisation.sql

--- BBD-Hopital/SQL/SQL-Automatisation.sql
+++ BBD-Hopital/SQL/SQL-Automatisation.sql
@@ -1,2 +1,6 @@
-create sequence autoinc
-  start with 1 increment by 1 1000;
+create sequence LIT_seq_AI
+  start with 1 increment by 1 nomaxvalue;
+create trigger LIT_AI before insert on LIT for each row
+begin
+  select LIT_seq_AI.nextval into:new.id from dual;
+end;
