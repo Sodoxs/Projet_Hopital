aaf4a11689f6a02725e075cde1d6d39f242da1cb
Jose Monteyraud <43368173+Jose-Monteyraud@users.noreply.github.com>
2018-11-14 16:55:17 +0100

    Update SQL-Peuplement.sql

--- BBD-Hopital/SQL/SQL-Peuplement.sql
+++ BBD-Hopital/SQL/SQL-Peuplement.sql
@@ -1,12 +1,24 @@
-INSERT INTO 
-
-INSERT INTO COMMANDE (ID,DATECOMMANDE,QUANTITECOMMANDE,IDETAT)
+DECLARE
+  x NUMBER := 0;
+BEGIN
+  LOOP
+    INSERT INTO MALADIE (ID,NOMMALADIE)
       VALUES(
-        dbms_random.value(11,25),
+        dbms_random.value(1,25),
+        dbms_random.string('A',100)
+        );
+    
+    INSERT INTO COMMANDE (ID,DATECOMMANDE,QUANTITECOMMANDE,IDETAT)
+      VALUES(
+        dbms_random.value(1,25),
         to_date('01/01/1980', 'DD/MM/YY') + trunc(dbms_random.value(0, (20*365))),
         dbms_random.value(100,200),
         dbms_random.value(1, 3)
         );
+    x := x + 1;  -- prevents infinite loop
+    EXIT WHEN x > 5;
+  END LOOP;
+END;
 commit;
 
 
