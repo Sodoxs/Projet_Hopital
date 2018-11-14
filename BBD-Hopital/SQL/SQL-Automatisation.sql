cc5a7cd3dd74fd4d3774e4d526a5f11a02d12b35
Guillaume GARCIA <37486861+Sodoxs@users.noreply.github.com>
2018-11-14 09:22:31 +0100

    Update SQL-Automatisation.sql

--- BBD-Hopital/SQL/SQL-Automatisation.sql
+++ BBD-Hopital/SQL/SQL-Automatisation.sql
@@ -1,7 +1,91 @@
-CREATE sequence LIT_seq_AI
+create sequence LIT_seq_AI
   start with 1 increment by 1 nomaxvalue;
 create trigger LIT_AI before insert on LIT for each row
 begin
   select LIT_seq_AI.nextval into:new.id from dual;
+end;
+
+create sequence PATIENT_seq_AI
+  start with 1 increment by 1 nomaxvalue;
+create trigger PATIENT_AI before insert on PATIENT for each row
+begin
+  select PATIENT_seq_AI.nextval into:new.id from dual;
+end;
+
+create sequence DATEMALADIE_seq_AI
+  start with 1 increment by 1 nomaxvalue;
+create trigger DATEMALADIE_AI before insert on DATEMALADIE for each row
+begin
+  select DATEMALADIE_seq_AI.nextval into:new.id from dual;
+end;
+
+create sequence MALADIE_seq_AI
+  start with 1 increment by 1 nomaxvalue;
+create trigger MALADIE_AI before insert on MALADIE for each row
+begin
+  select MALADIE_seq_AI.nextval into:new.id from dual;
+end;
+
+create sequence DATEMALADIE_seq_AI
+  start with 1 increment by 1 nomaxvalue;
+create trigger DATEMALADIE_AI before insert on DATEMALADIE for each row
+begin
+  select DATEMALADIE_seq_AI.nextval into:new.id from dual;
+end;
+
+create sequence DATEMALADIE_seq_AI
+  start with 1 increment by 1 nomaxvalue;
+create trigger DATEMALADIE_AI before insert on DATEMALADIE for each row
+begin
+  select DATEMALADIE_seq_AI.nextval into:new.id from dual;
+end;
+
+create sequence DATEMALADIE_seq_AI
+  start with 1 increment by 1 nomaxvalue;
+create trigger DATEMALADIE_AI before insert on DATEMALADIE for each row
+begin
+  select DATEMALADIE_seq_AI.nextval into:new.id from dual;
+end;
+
+create sequence DATEMALADIE_seq_AI
+  start with 1 increment by 1 nomaxvalue;
+create trigger DATEMALADIE_AI before insert on DATEMALADIE for each row
+begin
+  select DATEMALADIE_seq_AI.nextval into:new.id from dual;
+end;
+
+create sequence DATEMALADIE_seq_AI
+  start with 1 increment by 1 nomaxvalue;
+create trigger DATEMALADIE_AI before insert on DATEMALADIE for each row
+begin
+  select DATEMALADIE_seq_AI.nextval into:new.id from dual;
+end;
+
+create sequence DATEMALADIE_seq_AI
+  start with 1 increment by 1 nomaxvalue;
+create trigger DATEMALADIE_AI before insert on DATEMALADIE for each row
+begin
+  select DATEMALADIE_seq_AI.nextval into:new.id from dual;
+end;
+
+create sequence DATEMALADIE_seq_AI
+  start with 1 increment by 1 nomaxvalue;
+create trigger DATEMALADIE_AI before insert on DATEMALADIE for each row
+begin
+  select DATEMALADIE_seq_AI.nextval into:new.id from dual;
+end;
+
+create sequence DATEMALADIE_seq_AI
+  start with 1 increment by 1 nomaxvalue;
+create trigger DATEMALADIE_AI before insert on DATEMALADIE for each row
+begin
+  select DATEMALADIE_seq_AI.nextval into:new.id from dual;
+end;
+
+create sequence DATEMALADIE_seq_AI
+  start with 1 increment by 1 nomaxvalue;
+create trigger DATEMALADIE_AI before insert on DATEMALADIE for each row
+begin
+  select DATEMALADIE_seq_AI.nextval into:new.id from dual;
 end;
 commit;
