49fdebed5bb73cbb16546d9cefd358a0b5e9ee2d
Théotime <43368211+Theotime-Brault@users.noreply.github.com>
2018-11-15 14:44:57 +0100

    Update SQL-Requête.sql

--- BBD-Hopital/SQL/SQL-Requête.sql
+++ BBD-Hopital/SQL/SQL-Requête.sql
@@ -1,7 +1,15 @@
 --Guillaume est beau.
 
+-- Sélectionne le nom et le prenom des employés qui sont médecins et en pause.
 SELECT NOMEMPLOYE, PRENOMEMPLOYE FROM EMPLOYE
 WHERE IDROLE = (SELECT ID FROM ROLE WHERE NOMROLE = 'Medecin') 
     AND IDDISPONIBLE = (SELECT ID FROM DISPONIBLE WHERE DISPONIBLE = 'En pause');
 commit;
--- Sélectionne le nom et le prenom des employés qui sont médecins et en pause.
+
+-------------------------------------------------------------------------------
+-- Récupère l'id, la date et la quantité de médicament d'une commande
+SELECT ID, DATECOMMANDE, QUANTITECOMMANDE FROM COMMANDE 
+WHERE IDETAT = 3; -- 1 = à faire   2 = En cours   3 = Livré
+commit;
+
+-------------------------------------------------------------------------------
