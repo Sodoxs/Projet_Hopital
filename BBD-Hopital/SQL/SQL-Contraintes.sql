1148f3f236bec23c27ec9b09352c229abcf0ba3b
Florian Delgado <43368283+Ekinocte@users.noreply.github.com>
2018-11-15 14:21:08 +0100

    Update SQL-Contraintes.sql

--- BBD-Hopital/SQL/SQL-Contraintes.sql
+++ BBD-Hopital/SQL/SQL-Contraintes.sql
@@ -1,1 +1,24 @@
-Florian est gay.
+create function f_PATIENT_INOPERABLE() as
+    service_actu char;
+    patient INT;
+    service INT;
+  begin
+    select into patient ID from PATIENT where IDLIT = new.ID;
+    select into service IDSERVICE from PATIENT where IDPATIENT = patient;
+    select into service_actu SERVICE from SERVICE where SERVICE.ID = service;
+
+    if new.NUMBLOC != null and service_actu = 'chirugie' then
+      raise exception 'numero bloc ne peut pas etre null en service chirugie';
+    else if new.NUMBLOC = null and service_actu != 'chirugie' then
+      raise exception 'num bloc doit etre null pour a patient n etant pas en chirugie';
+    end if;
+
+    return new;
+end;
+
+create trigger PATIENT_INOPERABLE before update on  LIT for each row
+  declare
+  begin
+    execute procedure f_PATIENT_INOPERABLE();
+end;
+
