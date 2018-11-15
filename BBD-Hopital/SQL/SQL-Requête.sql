d7bc21a494df2f95c947a045653e29a4ff136b4e
Guillaume GARCIA <37486861+Sodoxs@users.noreply.github.com>
2018-11-15 14:40:37 +0100

    Update SQL-Requête.sql

--- BBD-Hopital/SQL/SQL-Requête.sql
+++ BBD-Hopital/SQL/SQL-Requête.sql
@@ -4,3 +4,4 @@ SELECT NOMEMPLOYE, PRENOMEMPLOYE FROM EMPLOYE
 WHERE IDROLE = (SELECT ID FROM ROLE WHERE NOMROLE = 'Medecin') 
     AND IDDISPONIBLE = (SELECT ID FROM DISPONIBLE WHERE DISPONIBLE = 'En pause');
 commit;
+-- Sélectionne le nom et le prenom des employés qui sont médecins et en pause.
