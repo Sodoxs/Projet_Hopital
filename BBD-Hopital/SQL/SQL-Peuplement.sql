c7a9dfb994b243bbc4bae294a0e72141b723c2b5
Guillaume GARCIA <37486861+Sodoxs@users.noreply.github.com>
2018-11-15 09:34:38 +0100

    Update SQL-Peuplement.sql

--- BBD-Hopital/SQL/SQL-Peuplement.sql
+++ BBD-Hopital/SQL/SQL-Peuplement.sql
@@ -38,7 +38,7 @@ BEGIN
     INSERT INTO COMMANDE (DATECOMMANDE,QUANTITECOMMANDE,IDETAT)
     VALUES
     (
-      to_date('01/01/1980', 'DD/MM/YY') + trunc(dbms_random.value(0, (20*365))),
+      SYSDATE,
       dbms_random.value(100,200),
       dbms_random.value(1, 3)
     );
@@ -62,7 +62,94 @@ BEGIN
       dbms_random.string('A',500),
       dbms_random.value(0,10000)
     );
+    
+    INSERT INTO PATIENT (NUMSECU, NUMMUTUELLE, CIVILITE, NOM, PRENOM, DATENAISSANCE, 
+                         NIVURGENCE, ADRESSE, DATEENTRE, DATESORTIE, TELEPHONE, IDLIT, IDSERVICE)
+    VALUES
+    (
+      dbms_random.value(1,9999999999),
+      dbms_random.value(1,9999999999),
+      dbms_random.string('A',1),
+      dbms_random.string('A',30),
+      dbms_random.string('A',30),
+      to_date('01/01/1980', 'DD/MM/YY') + trunc(dbms_random.value(0, (20*365))),
+      dbms_random.string('A',20),
+      SYSDATE,
+      SYSDATE,
+      dbms_random.string('A',15),
+      dbms_random.value(1,51),
+      dbms_random.value(1,51),
+      dbms_random.value(1,10)
+    );
+    
+    INSERT INTO EMPLOYE (LOGIN, NOM, PRENOM, MDP, IDROLE, IDDISPONIBLE)
+    VALUES
+    (
+      dbms_random.string('P',30),
+      dbms_random.string('A',30),
+      dbms_random.string('A',30),
+      dbms_random.string('P',50),
+      dbms_random.value(1,51),
+      dbms_random.value(1,51),
+    );
+    
+    x := x + 1;  -- prevents infinite loop
+    EXIT WHEN x > 50;
+  END LOOP;
+END;
+commit; -- lancer un nouveau script en adaptant la valeur max d'id commande.
+    
+DECLARE
+  x NUMBER := 0;
+BEGIN
+  LOOP
        
+    INSERT INTO INFECTER (IDPATIENT, IDMALADIE, IDEMPLOYE, IDDATE, DATEGUERISON)
+    VALUES
+    (
+      dbms_random.value(1,51),
+      dbms_random.value(1,51),
+      dbms_random.value(1,51),
+      dbms_random.value(1,51),
+      SYSDATE
+    );
+    
+    INSERT INTO TRAITEMENT (DATETRAITEMENT, DATEAPPLICATION, IDPATIENT, IDSTATUT)
+    VALUES
+    (
+      SYSDATE,
+      SYSDATE,
+      dbms_random.value(1,51),
+      dbms_random.value(1,51)
+    );
+    
+    x := x + 1;  -- prevents infinite loop
+    EXIT WHEN x > 50;
+  END LOOP;
+END;
+commit; -- lancer un nouveau script en adaptant la valeur max d'id commande.
+    
+DECLARE
+  x NUMBER := 0;
+BEGIN
+  LOOP
+  
+    INSERT INTO COMPOSER (IDMEDICAMENT, IDTRAITEMENT, QUANTITEMEDOC)
+    VALUES
+    (
+      dbms_random.value(1,51),
+      dbms_random.value(1,51),
+      dbms_random.value(0,1000)
+    );
+    
+    INSERT INTO GERER (IDEMPLOYE, IDTRAITEMENT, DATEPRESCRIPTION)
+    VALUES
+    (
+      dbms_random.value(1,51),
+      dbms_random.value(1,51),
+      SYSDATE
+    );
+    
     x := x + 1;  -- prevents infinite loop
     EXIT WHEN x > 50;
   END LOOP;
@@ -70,8 +157,9 @@ END;
 commit;
 
 
-//SYSDATE (pour mettre la date actuel)
-//to_date('01/01/1980', 'DD/MM/YY') + trunc(dbms_random.value(0, (20*365)))(date random)
-// pour un String dbms_random.string('A' ou 'X',taille) 
-//a pour Chaine de caractère Majuscule et minuscule
-//X pour une Chaine alpha-numériques
+--SYSDATE (pour mettre la date actuel)
+--to_date('01/01/1980', 'DD/MM/YY') + trunc(dbms_random.value(0, (20*365)))(date random)
+-- pour un String dbms_random.string('A' ou 'X',taille) 
+--a pour Chaine de caractère Majuscule et minuscule
+--X pour une Chaine alpha-numériques
+--P N'importe quelle caractère
