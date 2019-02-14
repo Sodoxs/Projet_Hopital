7852f6245d4dcfc043ad90ec14e953235226b011
tbraul01 <theotime98@hotmail.fr>
2019-02-14 15:10:50 +0100

    modified:   Site/src/Repository/GererRepository.php

--- Site/src/Repository/GererRepository.php
+++ Site/src/Repository/GererRepository.php
@@ -16,7 +16,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
 
 class GererRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry, string $entityClass)
+    public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, Gerer::class);
     }
