a7b2a6389821b0e7424a3337197017bb3dc581ac
tbraul01 <theotime98@hotmail.fr>
2019-02-15 12:09:55 +0100

    new file:   public/css/jquery-ui.min.css 	modified:   public/js/base.js 	new file:   public/js/jquery-ui.min.js 	modified:   templates/base.html.twig

--- Site/public/js/base.js
+++ Site/public/js/base.js
@@ -1,10 +1,9 @@
-import "jquery-3.3.1.min.js";
 
 $(document).ready(function () {
 
     //chrono message flash
-    $(function() {
+
         $('.message-flash').delay(2000).fadeOut();
-    });
+
 
 });
