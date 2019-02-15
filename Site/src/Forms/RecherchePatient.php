76a4bd106be73e41919091acd8c505db0e01d79c
tbraul01 <theotime98@hotmail.fr>
2019-02-15 11:55:05 +0100

    modified:   src/Controller/MedecinController.php 	modified:   src/Forms/RecherchePatient.php 	modified:   templates/SearchBar/SearchBar.html.twig

--- Site/src/Forms/RecherchePatient.php
+++ Site/src/Forms/RecherchePatient.php
@@ -4,11 +4,8 @@ namespace App\Forms;
 
 use App\Entity\Patient;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class RecherchePatient extends AbstractType
 {
