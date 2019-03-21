1a669ec116962a43e96016054b9d632f098909ca
MONTEYRAUD JOSE <jmonteyr@sfasp2mi.univ-poitiers.fr>
2019-03-21 14:47:19 +0100

    modified:   src/Controller/AccueilController.php 	modified:   src/Controller/GestionnaireController.php 	modified:   src/Controller/MedecinController.php 	modified:   src/Controller/PharmacienController.php

--- Site/src/Controller/AccueilController.php
+++ Site/src/Controller/AccueilController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * Class AccueilController
--- Site/src/Controller/MedecinController.php
+++ Site/src/Controller/MedecinController.php
@@ -15,6 +15,7 @@ use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 
 /**
