6fc81f2599f0ec377925e21eb5db008aeb8babf2
Ekinocte <florian.delgado4@gmail.com>
2019-03-21 16:35:13 +0100

    modifié :         src/Controller/AccueilController.php 	modifié :         templates/Accueil/accueil.html.twig

--- Site/src/Controller/AccueilController.php
+++ Site/src/Controller/AccueilController.php
@@ -36,36 +36,43 @@ class AccueilController extends AbstractController
         $patient->setEtaturgence('F');
 
         $lit = $em->getRepository('App:Lit')->findOneBy(['patient' => null]);
-        $patient->setLit($lit);
-        $lit->setPatient($patient);
+        if($lit){
+            $patient->setLit($lit);
+            $lit->setPatient($patient);
 
-        $form = $this->createForm(AjoutPatient::class, $patient, array(
-            'method' => 'POST'
-        ));
-        $form->add('submit', SubmitType::class, array('label' => 'Ajouter Patient',
-            'attr' => array(
-                'class' => "btn btn-contact",
-            )));
-        if ($request->isMethod('POST'))
-        {
-            if ($patient->getCivilite()=="0"){
-                $patient->setCivilite('H');
-            } else {
-                $patient->setCivilite('F');
-            }
-
-            $form->handleRequest($request);
-            if ($form->isSubmitted() && $form->isValid())
+            $form = $this->createForm(AjoutPatient::class, $patient, array(
+                'method' => 'POST'
+            ));
+            $form->add('submit', SubmitType::class, array('label' => 'Ajouter Patient',
+                'attr' => array(
+                    'class' => "btn btn-contact",
+                )));
+            if ($request->isMethod('POST'))
             {
-                $em->persist($patient);
-                $em->flush();
+                if ($patient->getCivilite()=="0"){
+                    $patient->setCivilite('H');
+                } else {
+                    $patient->setCivilite('F');
+                }
 
-                $this->addFlash('info', "Le patient a bien été ajouté !");
-                return $this->redirectToRoute('accueil'); // appel ajax to refresh
+                $form->handleRequest($request);
+                if ($form->isSubmitted() && $form->isValid())
+                {
+                    $em->persist($patient);
+                    $em->flush();
+
+                    $this->addFlash('info', "Le patient a bien été ajouté !");
+                    return $this->redirectToRoute('accueil'); // appel ajax to refresh
+                }
             }
+        } else {
+            $this->addFlash('info', "Aucun lit disponible !");
+            return $this->render('Accueil/accueil.html.twig', array('patients' => $patients,
+                'bool' => false));
         }
         return $this->render('Accueil/accueil.html.twig', array('form' => $form->createView(),
-            'patients' => $patients));
+            'patients' => $patients,
+            'bool' => true));
     }
 
 }
